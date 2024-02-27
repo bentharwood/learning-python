6ed52375ebe5f668ad5f6b463e10639092180ea3
bentharwood <titanharwood2016@gmail.com>
2024-02-27 02:42:36 -0500

    python game

--- rps/rps.py
+++ rps/rps.py
@@ -8,13 +8,16 @@ import webbrowser
 
 class App:
   def __init__(self):
-    global username
+    # setting up variables for play
+    global username 
     global wins
     global losses
+    global ties
+    ties = 0
+    wins = 0
+    losses = 0
     # create window
     self.root = tk.Tk()
-    # set window size
-    # self.root.geometry("800x400")
     # make window non-resizable
     self.root.resizable(False,False)
     # set window title
@@ -23,11 +26,12 @@ class App:
     mixer.init()
     # boolean variable to control if music plays
     self.music_playing = False
-    # setting up variables for play
+    #topbar frame
+    self.topbar = tk.Frame(self.root)
     # make top text for game
-    self.top_text = tk.Label(self.root, text="Rock! Paper! Scissors!", font=("helvetica", 18))
+    self.top_text = tk.Label(self.topbar, text="Rock! Paper! Scissors!", font=("helvetica", 18))
     # place text in window
-    self.top_text.pack(padx=10,pady=10)
+    self.top_text.pack(side="left",padx=10,pady=10)
     # resize musical-note.png to fit in music_button
     original_music_image = Image.open("assets/musical-note.png")
     resized_music_image = original_music_image.resize((20, 20), Image.Resampling.LANCZOS)
@@ -36,23 +40,25 @@ class App:
     original_no_music_image = Image.open("assets/music.png")
     resized_no_music_image = original_no_music_image.resize((20, 20), Image.Resampling.LANCZOS)
     self.no_music_icon = ImageTk.PhotoImage(resized_no_music_image)
-    # makes the button that pauses/unpauses the music then places it
-    self.music_button = tk.Button(self.root, image=self.music_icon,width=20,height=20, command=self.music_toggle)
-    self.music_button.pack(side="right",padx=5,pady=5)
+    # makes the button that pauses/unpauses the music then packs it
+    self.music_button = tk.Button(self.topbar, image=self.music_icon,width=20,height=20, command=self.music_toggle)
+    self.music_button.pack(side="left", padx=5,pady=5)
     # resizes info.png to fit the info button
     original_info_image = Image.open("assets/info.png")
     resized_info = original_info_image.resize((20, 20), Image.Resampling.LANCZOS)
     self.info_icon = ImageTk.PhotoImage(resized_info)
-    # makes the about RPS button and how to play then places it 
-    self.about_button = tk.Button(self.root, image=self.info_icon, width=20, height=20, command=self.info_toggle)
-    self.about_button.pack(side="right"padx=5,pady=5)
+    # makes the about RPS button and how to play then packs it 
+    self.about_button = tk.Button(self.topbar, image=self.info_icon, width=20, height=20, command=self.info_toggle)
+    self.about_button.pack(side="left", padx=5,pady=5)
     # resizes copyright.png to fit button
     original_copy_image = Image.open("assets/copyright.png")
     resized_copy = original_copy_image.resize((20, 20), Image.Resampling.LANCZOS)
     self.copy_icon = ImageTk.PhotoImage(resized_copy)
-    # makes the copyright button then places it
-    self.copyright_button = tk.Button(self.root, image=self.copy_icon, width=20, height=20, command=self.copyright_toggle)
-    self.copyright_button.pack(side="right", padx=5,pady=5)
+    # makes the copyright button then packs it
+    self.copyright_button = tk.Button(self.topbar, image=self.copy_icon, width=20, height=20, command=self.copyright_toggle)
+    self.copyright_button.pack(side="left", padx=5,pady=5)
+    #displays topbar
+    self.topbar.pack()
     # setting up starting menu frame
     self.startScreen = tk.Frame(self.root)
     # loads start menu music
@@ -79,21 +85,38 @@ class App:
     paper_img_resized = paper_img_raw.resize((125, 100), Image.Resampling.LANCZOS)
     scissors_img_resized = scissors_img_raw.resize((125, 100), Image.Resampling.LANCZOS)
     # imgs ready for cards
-    rock_img = ImageTk.PhotoImage(rock_img_resized)
-    paper_img = ImageTk.PhotoImage(paper_img_resized)
-    scissors_img = ImageTk.PhotoImage(scissors_img_resized)
+    self.rock_img = ImageTk.PhotoImage(rock_img_resized)
+    self.paper_img = ImageTk.PhotoImage(paper_img_resized)
+    self.scissors_img = ImageTk.PhotoImage(scissors_img_resized)
     # creating cards for play
-    self.rock= tk.Button(self.game_state, borderwidth=2, width=150, height=150, relief="solid",image=rock_img)
-    self.paper= tk.Button(self.game_state, borderwidth=2, width=150, height=150, relief="solid",image=paper_img)
-    self.scissors= tk.Button(self.game_state, borderwidth=2, width=150, height=150, relief="solid",image=scissors_img)
+    self.buttonFrame = tk.Frame(self.game_state)
+    self.rock= tk.Button(self.buttonFrame, borderwidth=2, width=150, height=150, relief="solid",image=self.rock_img, command=lambda:self.play_game("R") )
+    self.paper= tk.Button(self.buttonFrame, borderwidth=2, width=150, height=150, relief="solid",image=self.paper_img, command=lambda:self.play_game("P"))
+    self.scissors= tk.Button(self.buttonFrame, borderwidth=2, width=150, height=150, relief="solid",image=self.scissors_img, command=lambda:self.play_game("S"))
+    self.buttonFrame.pack()
     # Pack buttons side by side with vertical centering
     self.rock.pack(side="left", padx=5, pady=5, anchor="center")
     self.paper.pack(side="left", padx=5, pady=5, anchor="center")
     self.scissors.pack(side="left", padx=5, pady=5, anchor="center")
+    #score counter
+    self.score_frame = tk.Frame(self.game_state)
+    self.score = tk.Label(self.score_frame, text=f"{wins} WINS {losses} LOSSES {ties} TIES", font=("helvetica", 18))
+    self.score.grid(row=1, column=1, pady=10, sticky="nsew")
+    self.score_frame.pack(expand=True)
     #results screen
-    results_screen = tk.Frame(self.root)
-    winner = tk.Label(results_screen, text="placeholder")
-    loser = tk.Label(results_screen, text="placeholder")
+    self.results_screen = tk.Frame(self.root)
+    self.winner_comp = tk.Frame(self.results_screen)
+    self.winner_comp.pack()
+    self.user_choice_label = tk.Label(self.winner_comp, text="placeholder")
+    self.comp_choice_label = tk.Label(self.winner_comp, text="placeholder")
+    self.winner_comparison = tk.Label(self.winner_comp, text="/")
+    self.user_choice_label.pack(side="left", padx=5, pady=5, anchor="center")
+    self.winner_comparison.pack(side="left", padx=5, pady=5, anchor="center")
+    self.comp_choice_label.pack(side="left", padx=5, pady=5, anchor="center")
+    self.retry_frame = tk.Frame(self.results_screen)
+    self.retry_frame.pack()
+    self.retry_button = tk.Button(self.retry_frame,borderwidth=2,relief="solid", text="Play Again", command=lambda: self.retry())
+    self.retry_button.pack(padx=10,pady=10)
     # asks if your ar sure you want to quit
     self.root.protocol("WM_DELETE_WINDOW", self.on_close)
     self.root.mainloop()
@@ -102,14 +125,33 @@ class App:
   def on_close(self):
     if(messagebox.askyesno(title="quit?", message="Do you really want to quit>")):
       self.root.destroy()
-
   # starts game by calling game_state_frame and hiding start_menu_frame 
-  def calls_game_state(self):
-    if(self.username_box):
-      pass
-
+  def play_game(self, choice):
+    global ties, wins, losses  
+    player_choice = choice
+    computer_choice = self.comp_choice()
+    # Map choices to corresponding images
+    choice_images = {"R": self.rock_img, "P": self.paper_img, "S": self.scissors_img}
+    self.user_choice_label.config(image=choice_images[player_choice])
+    self.comp_choice_label.config(image=choice_images[computer_choice])
+    if player_choice == computer_choice:
+      self.winner_comparison.config(text=" = ", font=("helvetica", 32))
+      ties += 1
+      self.top_text.config(text=f"ITS A TIE! would you like to play again?")
+    elif (player_choice == "R" and computer_choice == "S") or \
+        (player_choice == "P" and computer_choice == "R") or \
+        (player_choice == "S" and computer_choice == "P"):
+      self.winner_comparison.config(text=" > ", font=("helvetica", 32))
+      wins += 1
+      self.top_text.config(text=f"YOU WON! would you like to play again?")
+    else:
+      self.winner_comparison.config(text=" < ", font=("helvetica", 32))
+      losses += 1
+      self.top_text.config(text=f"YOU LOST! would you like to play again?")
+    self.game_state.pack_forget()
+    self.results_screen.pack()
+    self.score.config(text=f"{wins} WINS {losses} LOSSES {ties} TIES", font=("helvetica", 18))
   # music functions
-  
   def pause_music(self):
     self.music_playing = False
     mixer.music.pause()
@@ -156,13 +198,14 @@ class App:
     music_hyperlink.bind("<Button-1>", lambda event: self.open_link("https://www.chosic.com/download-audio/25860/"))
 
   def start_game(self):
+    global username
     username= self.username_box.get()
     if username and username != "username:":
         # If username_box is not empty, save the username and proceed
         self.username = username
         self.top_text.config(text=f"Welcome {username}, please chose a card!")
         self.startScreen.pack_forget()
-        self.game_state.pack()
+        self.game_state.pack(padx=10, pady=10)
         mixer.music.unload()
         mixer.music.load("assets/game_state_music.mp3")
         mixer.music.play(-1)
@@ -171,7 +214,12 @@ class App:
         # If username_box is empty, show an error message
         messagebox.showerror("Error", "Please enter a username to start the game.")
   
-  def comp_choice():
+  def retry(self):
+    global username
+    self.results_screen.pack_forget()
+    self.game_state.pack(padx=10, pady=10)
+    self.top_text.config(text=f"Welcome back {username}, Thank you for playing again!")
+  def comp_choice(self):
       choice = random.choice([0, 1, 2])
       if choice == 0:
         return 'R'
@@ -179,5 +227,5 @@ class App:
         return 'P'
       else:
          return 'S'
-
+      
 App()
